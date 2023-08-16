3c32b0eff55fb5c46659b2111f563d07c585875a
ritacassiasr@gmail.com <ritacassiasr@gmail.com>
2023-08-16 19:07:54 -0400

    chore: construindo a lógica do caixa

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,17 +1,43 @@
 import { Menu } from "./menu";
 
 class CaixaDaLanchonete {
-    constructor() {
-        this.menu = new Menu();
-    }
-    
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        let total = o;
+  constructor() {
+    this.menu = new Menu();
+  }
 
-        if(itens.length === 0) return "Não há itens no carrinho de compra!"
-        if(!['debito', 'credito','dinheiro'].includes(metodoDePagamento)) return "Forma de pagamento inválida!";
+  calcularValorDaCompra(formaDePagamento, itens) {
+    let total = 0;
+
+    if (itens.length === 0) return "Não há itens no carrinho de compra!";
+    if (!["debito", "credito", "dinheiro"].includes(formaDePagamento))
+      return "Forma de pagamento inválida!";
+
+    for (let item of itens) {
+      const [codigo, quantidade] = item.split(",");
+
+      const menuItem = this.menu.getItem(codigo);
+      if (!menuItem) return "Item inválido!";
+
+      if (Number(quantidade) <= 0) return "Quantidade inválida!";
+
+      if (
+        this.menu.isExtra(codigo) &&
+        !itens.some((i) => i.startsWith(this.menu.getPrincipalForExtra(codigo)))
+      ) {
+        return "Item extra não pode ser pedido sem o principal";
+      }
+
+      total += menuItem.valor * Number(quantidade);
     }
 
+    if (formaDePagamento === "dinheiro") {
+      total *= 0.95;
+    } else if (formaDePagamento === "credito") {
+      total *= 1.03;
+    }
+
+    return `R$ ${total.toFixed(2).replace(".", ",")}`;
+  }
 }
 
 export { CaixaDaLanchonete };
