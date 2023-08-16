7dd78978651c00819da832b3ab0b79923f291522
ritacassiasr@gmail.com <ritacassiasr@gmail.com>
2023-08-16 18:17:20 -0400

    chore: criando a classe menu, para gerenciar o cardápio

---
+++ src/menu.js
@@ -0,0 +1,33 @@
+class Menu {
+    constructor() {
+        this.items = {
+            cafe: { descricao: "Café", valor: 3.00 },
+            chantily: { descricao: "Chantily (extra do Café)", valor: 1.50 },
+            suco: { descricao: "Suco Natural", valor: 6.20 },
+            sanduiche: { descricao: "Sanduíche", valor: 6.50 },
+            queijo: { descricao: "Queijo (extra do Sanduíche)", valor: 2.00 },
+            salgado: { descricao: "Salgado", valor: 7.25 },
+            combo1: { descricao: "Suco e 1 Sanduíche", valor: 9.50 },
+            combo2: { descricao: "Café e 1 Sanduíche", valor: 7.50 }
+        };
+
+        this.extras = {
+            chantily: "cafe",
+            queijo: "sanduiche"
+        };
+    }
+
+    getItem(codigo) {
+        return this.items[codigo] || null;
+    }
+
+    isExtra(codigo) {
+        return !!this.extras[codigo];
+    }
+
+    getPrincipalForExtra(codigo) {
+        return this.extras[codigo];
+    }
+}
+
+export { Menu }
